b34ccb2918ce6b1289fa80527a74e6033a96cf08
Andrei <andrei@localhost.localdomain>
2021-01-05 18:43:35 +0200

    made button

--- Node2D.gd
+++ Node2D.gd
@@ -1,16 +1,31 @@
 extends Node2D
 
-var hp = 20
+var hp = 500
 var player_name = "Isaac"
+var power = 1
+var coins = 0
+var price = 1
 
 func _ready():
 	$Label.text = ("You %s have %d HP to start with." % [player_name, hp])
+
 func _on_Button_pressed():
 	if hp > 0:
-		hp -= 1
+		hp -= power
+		coins += 1
 	if(hp <= 0):
 		$Label.text = ("You Died")
 	else:
 		$Label.text = ("%d HP left." % [hp])
+		
+func _on_addpower_pressed():
+	if coins >= price:
+		power += 1
+		coins -= price
+		price += (power+5)*2
+
 func _process(delta):
-	pass
+#	$Coins.text = str(coins)
+#	$Price.text = str(price)
+	$Price.text = ("Price %d" % [price])
+	$Coins.text = ("Coins %d" % [coins])
