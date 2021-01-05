ad9c4286d61e653db1e0048005f8d4c69eead35d
GhokyTheOne <tincescu15@gmail.com>
2021-01-05 17:20:34 +0200

    saru mana
    masteeeeeeeeeeeeeeeer

--- Node2D.gd
+++ Node2D.gd
@@ -3,12 +3,14 @@ extends Node2D
 var hp = 20
 
 func _ready():
-	pass
-func _on_Enemy_deal_damage():
-	hp -= 1
-
+	print("your Hp is:",hp) 
+	
 func _on_Button_pressed():
-	hp -= 1
-
+	if hp > 0:
+		hp -= 1
+	if(hp <= 0):
+		print("u lose")
+	else:
+		print(hp)
 func _process(delta):
-	print(hp)
+	pass
