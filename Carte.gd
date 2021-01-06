4832a8aa5b9a7609c4e983460fded4132592ee75
Andrei <andrei@localhost.localdomain>
2021-01-06 14:47:17 +0200

    signals

--- Carte.gd
+++ Carte.gd
@@ -1,8 +1,5 @@
 extends Node2D
-signal spawn_man
 
 func _ready():
 	randomize()
 	position = Vector2(rand_range(0,500), rand_range(0,500))
-func _process(delta):
-	emit_signal("spawn_man")
--- Node2D.gd
+++ Node2D.gd
@@ -1,33 +1,39 @@
 extends Node2D
 
-
-var value = "Hello, I am genius, it works!"
-signal cardvalue(value)
-const Carte = preload("res://Carte.tscn")
 var cards = [2, 3, 4, 5, 6, 7, 8, 9, 10,"A","J","Q","K"]
 var colors = ["trefla","inima","inima neagra","romb"]
-onready var Card = get_node("Card").get_node("Label")
+var value = "Hello, I am genius, it works!"
+var i = 0
+# First we signal the cardvalue.
+signal cardvalue(value)
+# Preload the instance.
+const Carte = preload("res://Carte.tscn")
+# Set a variable with the position of the Label node.
+onready var Card = get_node("root/Node2D/Card/Label")
 
 func _ready():	
-	$Card.connect("spawn_man", self, "_on_Card_spawn_man")
-	var i = 0
+	i = 0
 	
-	while i < 1:
+	while i < 4:
 		i += 1
 		randomize()
 		
-		var card = round(rand_range(0,13))
+		var card = round(rand_range(0,12))
 		var color = round(rand_range(0,3))
+		var cardcolor = colors[color]
+		var cardsymbol = cards[card]
 		var cardvalue = card + 2
-		var hand =[]
 		
+		# Create an instance as child.
 		var GrabedInstance= Carte.instance()
 		self.add_child(GrabedInstance)
 		
-		print(cards[card])
-		print(colors[color])
+		print(cardsymbol)
+		print(cardcolor)
 		print("Card value is", cardvalue)
+		value = ("%s de %s" % [cardsymbol, cardcolor])
+		# Need to change name and send signal to only 1 card.
+		# Set node location to be personal.
+		# Send value just to that one.
+		# After ading instance you can conect or emit signal with value.
 		emit_signal("cardvalue", value)
-
-func _on_Card_spawn_man():
-	pass
