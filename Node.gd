801117f6485c299e62433fd9d04d2826b0a9a85d
GhokyTheOne <tincescu15@gmail.com>
2021-01-07 18:21:54 +0200

    senpai
    traduce-mi in pastele mati :))))))))))

--- Node.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-
-func _ready():
-	self. = Vector2(rand_range(0,500), rand_range(0,500))
--- Node2D.gd
+++ Node2D.gd
@@ -1,15 +1,22 @@
 extends Node2D
 
+#var card[100]
+#var color[100]
+
 var cards = [2, 3, 4, 5, 6, 7, 8, 9, 10,"A","J","Q","K"]
 var colors = ["trefla","inima","inima neagra","romb"]
 var value = "Hello, I am genius, it works!"
 var i = 0
 # First we signal the cardvalue.
+
 signal cardvalue(value)
 # Preload the instance.
+
 const Carte = preload("res://Carte.tscn")
 # Set a variable with the position of the Label node.
+
 onready var Card = get_node("root/Node2D/Card/Label")
+
 
 func _ready():	
 	i = 0
@@ -17,12 +24,15 @@ func _ready():
 	while i < 4:
 		i += 1
 		randomize()
-		
+		###########
 		var card = round(rand_range(0,12))
 		var color = round(rand_range(0,3))
 		var cardcolor = colors[color]
 		var cardsymbol = cards[card]
 		var cardvalue = card + 2
+		#################
+		#card[i] = round(rand_range(0,12))
+		#color[i] = round(rand_range(0,3))
 		
 		# Create an instance as child.
 		var GrabedInstance= Carte.instance()
